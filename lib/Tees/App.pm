cfabf97ee2293c72263a8cda5bbd2f0804219582
Claire <claire.hernandez@biblibre.com>
2011-06-26 15:08:36 +0200

    First use case very basic
    - add a model (only a title)
    - save it in a file
    - redirect to a list of models wich read a file

--- lib/Tees/App.pm
+++ lib/Tees/App.pm
@@ -7,4 +7,30 @@ get '/' => sub {
     template 'index';
 };
 
+get '/new/model' => sub {
+    template 'newmodel';
+};
+
+post '/new/model' => sub {
+    add_model (params->{title});
+    return redirect '/list/models';
+};
+
+sub list_models {
+  open (MODELS, "models.log");
+  my @lignes = <MODELS>;
+}
+
+get '/list/models' => sub {
+    my @models = list_models;
+    return template 'listmodels' => {models => \@models};
+};
+
+sub add_model {
+  my ($title) = @_;
+  open (MODELS, ">>models.log");
+  print MODELS "$title\n";
+  close MODELS;
+}
+
 true;
